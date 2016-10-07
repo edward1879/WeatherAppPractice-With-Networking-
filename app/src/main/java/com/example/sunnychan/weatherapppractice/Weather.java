74a427f589330260c8f84eb77f62f944ba89cb12
SunnyChan <edward1879314@gmail.com>
2016-10-07 15:13:15 +0800

    Added the weather model

--- app/src/main/java/com/example/sunnychan/weatherapppractice/Weather.java
+++ app/src/main/java/com/example/sunnychan/weatherapppractice/Weather.java
@@ -5,5 +5,50 @@ package com.example.sunnychan.weatherapppractice;
  */
 
 public class Weather {
+    private String weatherDescription;
+    private String date;
+    private String low;
+    private String high;
 
+    public Weather() {
+    }
+
+    public Weather(String weatherDescription, String date, String low, String high) {
+        this.weatherDescription = weatherDescription;
+        this.date = date;
+        this.low = low;
+        this.high = high;
+    }
+
+    public String getWeatherDescription() {
+        return weatherDescription;
+    }
+
+    public void setWeatherDescription(String weatherDescription) {
+        this.weatherDescription = weatherDescription;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLow() {
+        return low;
+    }
+
+    public void setLow(String low) {
+        this.low = low;
+    }
+
+    public String getHigh() {
+        return high;
+    }
+
+    public void setHigh(String high) {
+        this.high = high;
+    }
 }
